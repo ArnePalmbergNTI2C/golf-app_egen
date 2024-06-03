9a06fb787d4b3a34d2bda53526efa8709f1f1ab9
ArnePalmbergNTI2C <150041090+ArnePalmbergNTI2C@users.noreply.github.com>
2024-06-03 23:16:11 +0200

    klar med första kvälls verisionen

---
+++ golf/main.rb
@@ -0,0 +1,117 @@
+def switch(array, array2, array3)
+
+    endgame = array3
+
+    i = 0
+    output = []
+
+    while i < array.length
+
+        x = 0
+
+        while x < array2.length
+    
+            j = 0
+            string = ""
+    
+            while j < array2[x].length
+
+                array2[x] = array2[x].sub(/^\d+\s*/, '')
+
+                if array2[x][j] == array[i][j] 
+                    string << array[i][j]
+                end
+
+                j += 1
+
+            end
+   
+
+            if string.length == array[i].length
+
+                output << endgame[x]
+            
+    
+            end
+            x +=1
+
+        end
+    
+        i += 1
+
+    end
+
+    return output
+
+end
+
+def Mats(array)
+
+    new_array = []
+    
+    while array.length > 0
+        i = 0
+        minsta = 1000000
+        while i < array.length
+
+            x = 0
+
+            sum = ""
+            bob = 0
+
+
+            while x < array[i].length
+
+
+                j = 0
+
+                while j < 10
+
+                    if array[i][x] == j.to_s
+
+                        sum << array[i][x]
+                        bob += sum.to_f
+
+                    end
+
+                    j += 1
+
+                end
+
+                x += 1
+
+            end
+
+
+            if bob < minsta
+                minsta = bob 
+                temp = i
+            end
+
+
+
+            i += 1
+        end
+        new_array << array[temp]
+        array.delete_at(temp)
+
+    end 
+
+    return new_array
+
+end
+
+array3 = File.readlines("golf_data.txt").map { |line| line.gsub(/[\t]/, ' ') }
+array2 = File.readlines("golf_data.txt").map { |line| line.gsub(/[\t]/, ' ') }
+array = File.readlines("personer.txt")
+
+slut = switch(array, array2, array3)
+slut_fil = File.open("input.txt", "w")
+slut_fil.puts(slut)
+slut_fil.close
+
+array = File.readlines("input.txt")
+bob = Mats(array)
+fil = File.open("output.txt", "w")
+fil.puts(bob)
+fil.close
